f9286017a9046d6f0b8c2b6708dfd9f481fe3e96
Marina Solovieva <marishasolovieva@mail.ru>
2020-06-15 00:30:38 +0300

    add new project

---
+++ src/context/alert/alertReducer.js
@@ -0,0 +1,13 @@
+import {HIDE_ALERT, SHOW_ALERT} from "../types";
+
+const handlers = {
+    [SHOW_ALERT]: (state, {payload}) => ({...payload, visible: true}),
+    [HIDE_ALERT]: state => ({...state, visible: false}),
+    DEFAULT: state => state
+};
+
+
+export const AlertReducer = (state, action) => {
+    const handler = handlers[action.type] || handlers.DEFAULT;
+    return handler(state, action)
+};
